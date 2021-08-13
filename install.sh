fe70e2a31be416e9800579153f7e0f42fcb7a03a
Max Bickers <mbickers@umich.edu>
2021-08-12 22:39:40 -0500

    Moved stuff to use stow

--- install.sh
+++
@@ -1,8 +0,0 @@
-ln -sf ~/dotfiles/git/.gitignore ~/.gitignore
-ln -sf ~/dotfiles/git/.gitconfig ~/.gitconfig
-ln -sf ~/dotfiles/.hushlogin ~/.hushlogin
-ln -sf ~/dotfiles/zshrc ~/.zshrc
-ln -sf ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
-rm -r ~/.config
-mkdir -p ~/.config
-ln -sf ~/dotfiles/vim ~/.config/nvim
